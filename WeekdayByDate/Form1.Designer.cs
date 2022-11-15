4bac2a12a273b9eb0ea4ff5a31e704f59cca679f
Sergey Evseev <sevseev@gmail.com>
2022-11-16 00:54:23 +0300

    buttons

--- WeekdayByDate/Form1.Designer.cs
+++ WeekdayByDate/Form1.Designer.cs
@@ -28,21 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(88, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Define week day by entered date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите дату через пробел  (DD MM YYYY)";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(138, 74);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(135, 25);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "или выберите дату";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(138, 154);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(135, 25);
+            this.dateTimePicker.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "День недели введенной даты:";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.resultTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.resultTextBox.Location = new System.Drawing.Point(138, 250);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(135, 25);
+            this.resultTextBox.TabIndex = 6;
+            this.resultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonOK.Location = new System.Drawing.Point(63, 295);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(101, 35);
+            this.buttonOK.TabIndex = 7;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonClear.Location = new System.Drawing.Point(228, 295);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(101, 35);
+            this.buttonClear.TabIndex = 8;
+            this.buttonClear.Text = "CLEAR";
+            this.buttonClear.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 342);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Weekday";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox inputTextBox;
+        private Label label3;
+        private DateTimePicker dateTimePicker;
+        private Label label4;
+        private TextBox resultTextBox;
+        private Button buttonOK;
+        private Button buttonClear;
     }
 }
